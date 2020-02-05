9992d308c0133ea438427e09c5f945c51a03f6f5
Pranav <pranav.developer@outlook.com>
2020-02-05 22:16:42 +0530

    Initial Commit

---
+++ GettingStartedDemo/Application.Tests/GetCustomerQueryTests.cs
@@ -0,0 +1,65 @@
+﻿using Application.Customers.Queries;
+using Application.Domain;
+using Application.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Xunit;
+
+namespace Application.Tests
+{
+    public class GetCustomerQueryTests
+    {
+
+        [Fact]
+        public void ShouldReturnAllCustomers()
+        {
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                .UseInMemoryDatabase(databaseName: "ShouldReturnAllCustomers")
+                .Options;
+
+            var context = new NorthwindContext(options);
+
+            Seed(context);
+
+            var query = new GetCustomersQuery(context);
+
+            var result = query.Execute();
+
+            Assert.Equal(6, result.Count);
+        }
+
+        [Fact]
+        public void ShouldOrderCustomersByName()
+        {
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                .UseInMemoryDatabase(databaseName: "ShouldOrderCustomersByName")
+                .Options;
+
+            var context = new NorthwindContext(options);
+
+            Seed(context);
+
+            var query = new GetCustomersQuery(context);
+
+            var result = query.Execute();
+
+            Assert.Equal("Beth Smith", result.First().Name);
+        }
+
+        private void Seed(NorthwindContext context)
+        {
+            var customers = new[]
+            {
+                new Customer { Name = "Jerry Smith" },
+                new Customer { Name = "Summer Smith" },
+                new Customer { Name = "Morty Smith" },
+                new Customer { Name = "Rick Sanchez" },
+                new Customer { Name = "Beth Smith" },
+                new Customer { Name = "Mr. Meeseeks" },
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# IntegrariontestingDotnetCore
+# Simplified Unit Testing with the EF Core InMemory Provider 
+The slides and demo code for my talk at NDC Sydney 2017
